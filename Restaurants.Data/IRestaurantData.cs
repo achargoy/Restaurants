de03761357dee5258c1fa37dc3b12d4e46a7480a
Alberto Chargoy <chargoy@Albertos-MacBook-Air.local>
2022-01-18 13:44:29 -0500

    Adding Restaurants solution

---
+++ Restaurants.Data/IRestaurantData.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Restaurants.Core;
+
+namespace Restaurants.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant(){ Id=1, Name="Dominos", CuisineType = CuisineType.None, Location="FairFax" },
+                new Restaurant(){ Id=2, Name="McDonalds", CuisineType = CuisineType.None, Location="Meryland" },
+                new Restaurant(){ Id=3, Name="Subway", CuisineType = CuisineType.None, Location="FairFax" }
+            };
+        }
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   select r;
+        }
+    }
+}
