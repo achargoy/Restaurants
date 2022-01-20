74300958e0e3c6b9d2e06db42247767e45ca529d
achargoy <achargoy@hotmail.com>
2022-01-20 16:15:49 -0500

    Building a Create Restaurant Page

--- Restaurants/Pages/Restaurants/Edit.cshtml.cs
+++ Restaurants/Pages/Restaurants/Edit.cshtml.cs
@@ -26,10 +26,18 @@ namespace Restaurants.Pages.Restaurants
             this.htmlHelper = htmlHelper;
         }
 
-        public IActionResult OnGet(int restaurantId)
+        public IActionResult OnGet(int? restaurantId)
         {
             Cuisines = htmlHelper.GetEnumSelectList<CuisineType>();
-            Restaurant = restaurantData.GetById(restaurantId);
+
+            if (restaurantId.HasValue)
+            {
+                Restaurant = restaurantData.GetById(restaurantId.Value);
+            }
+            else
+            {
+                Restaurant = new Restaurant();
+            }
 
             if (Restaurant == null)
             {
