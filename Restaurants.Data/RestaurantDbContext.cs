8a19cd3ad5eab81b2b4f5670fb636bcbcd049037
achargoy <achargoy@hotmail.com>
2022-01-22 21:27:50 -0500

    Implementing an Entity Framework DbContext

---
+++ Restaurants.Data/RestaurantDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Restaurants.Core;
+
+namespace Restaurants.Data
+{
+    public class RestaurantDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
